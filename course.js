aa2171c7f95b3ff0a170db301a41144c56ea9188
Damian Galeano <dj.damian.b@hotmail.com>
2022-11-18 20:57:12 +0100

    inicial

---
+++ course.js
@@ -0,0 +1,51 @@
+var ageOne = 12;
+
+ageOne.toString();
+
+
+
+var number = "12";
+
+parseInt(12);
+
+
+console.log(number);
+
+
+
+var str = "the quick brown fox jumped over the lazy dog"
+
+console.log(str.charAt(2));
+
+
+console.log(str.concat("again and again"));
+
+str.includes("lazy");
+console.log(str.includes("lazy"));
+
+
+console.log(str.startsWith("the"));
+
+console.log(str.endsWith("dog"));
+
+console.log(str.match());
+
+
+console.log(str.replace("fox", "horse"));
+
+
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+
+
+
+var messyString = "          hy _there     ";
+console.log(messyString.trim());
+
+
+
+
+
+
+
+
