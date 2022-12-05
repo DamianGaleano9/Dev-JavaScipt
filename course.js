b7238f396c41e175c6539c3e57976230a7de07c3
Damian Galeano <dj.damian.b@hotmail.com>
2022-12-05 23:56:34 +0100

    switch

--- course.js
+++ course.js
@@ -1528,4 +1528,27 @@ CODIGO | RESULT
 
     // console.log(interPretIMC(24));
 
-    
+// function clasificarVolumen (valor) {
+//     var volumen;
+//         switch (valor) {
+//         case 1: 
+//         volumen = "Bajo";
+//         break;
+//         case 2:
+//         case 3:
+//         volumen =  "Intermedio";
+//         break;
+//         case 4: 
+//         case 5: 
+//         case 6: 
+//         volumen = "Alto";
+//         break;
+//     }
+
+//     return volumen;
+// }
+
+// console.log(clasificarVolumen(6));
+
+
+
